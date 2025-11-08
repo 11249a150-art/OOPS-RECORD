dad9a5bce006b0775ceb08eb210d04b15b58cb41
11249a150-art <11249a150@kanchiuniv.ac.in>
2025-11-07 22:21:00 -0800

    Update overloadingassignment.cpp

--- overloadingassignment.cpp
+++ overloadingassignment.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
-class Account {
+class Book {
+char title[50];
+float price;
 public:
-virtual void displayBalance() {
-cout << "Generic account balance.\n";
+void getData() {
+cout << "Enter book title: ";
+
+cin >> title;
+cout << "Enter price: ";
+cin >> price;
 }
-};
-class Savings : public Account {
-public:
-void displayBalance() {
-cout << "Savings Account Balance: ₹5000\n";
+void operator=(Book &b) {
+strcpy(title, b.title);
+price = b.price;
 }
-};
-class Current : public Account {
-public:
-void displayBalance() {
-cout << "Current Account Balance: ₹10000\n";
+void display() {
+cout << "Title: " << title << ", Price: " << price << endl;
 }
 };
 int main() {
-Account *acc;
-Savings s;
-Current c;
-acc = &s; acc->displayBalance();
-acc = &c; acc->displayBalance();
+Book b1, b2;
+b1.getData();
+b2 = b1;
+cout << "\nCopied Book Details:\n";
+b2.display();
+return 0;
 
 }
